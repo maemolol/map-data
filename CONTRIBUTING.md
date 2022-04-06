ed730c0ae23f8f1dec1ec7ab1aed183440e97047
7d <i.second.7d@gmail.com>
2022-04-06 20:15:48 +0800

    update tiles to cover 64 instead of 32

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,13 +5,16 @@ To start mapping, please join MRT-Map's Discord server and ping @7d#5755 for the
 **DO NOT** push to the main branch or development branch (only 7d can cos he knows the standard :D); fork this repo and write the changes there, and also create a pull request for merging into the `development` branch. Optionally (but please do for your first time mapping), consult 7d to check if the standard is correct before pushing. Once a namespace is complete, your fork will then be merged into `main` at the discretion of 7d, and these changes will be deployed to the map itself.
 
 # List of namespaces
-* lke: large lakes
+* lke: large lakes - ava
 * srd: A/B roads
-* mrt: mrt lines
+* mrt: mrt lines - ava
 * enc: encinitas - __7d
+* ast: aston - notshort275
 * cct: central city
 * rlq: railinq
 * brl: blurail
 * irl: intrarail
 * nrl: newrail
-* frl: fred rail
+* frl: fred rail 
+* swh: southwold
+* cyp: cypress - ava
---
+++ render.py
@@ -0,0 +1,38 @@
+import json
+import os
+import re
+import shutil
+from glob import glob
+from pathlib import Path
+
+import renderer
+from tqdm import tqdm
+
+
+def read_file(path) -> dict:  # extract from JSON as dict
+    with open(path, "r") as f:
+        data = json.load(f)
+        return data
+
+def main():
+    node_json = {}
+    for node_file in glob("nodes/*"):
+        node_json.update(read_file(node_file))
+    nodes = renderer.NodeList(node_json)
+
+    comps_json = {}
+    for comp_file in glob("comps/*"):
+        comps_json.update(read_file(comp_file))
+    comps = renderer.ComponentList(comps_json, node_json)
+    renderer.render(comps, nodes, 8, 8, 64, save_dir=Path("./tiles"))
+
+    for tile in tqdm(glob("tiles/*")):
+        tile: str
+        regex = re.search(r"tiles/(-?\d+), (-?\d+), (-?\d+)\.png", tile)
+        if regex is None: continue
+        z, x, y = regex.group(1), regex.group(2), regex.group(3)
+        Path(f"./tiles/{z}/{x}").mkdir(parents=True, exist_ok=True)
+        shutil.move(tile, Path(f"./tiles/{z}/{x}"))
+        os.rename(f"./tiles/{z}/{x}/"+tile.removeprefix("tiles/"), f"./tiles/{z}/{x}/{y}.png")
+
+if __name__ == "__main__": main()
