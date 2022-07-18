257732d2511b1d061f33d09b68aa574f2eea08d8
7d <i.second.7d@gmail.com>
2022-07-18 11:01:36 +0800

    render wyg, pnb, gep

--- render.py
+++ render.py
@@ -1,38 +1,31 @@
-import gc
 import json
 from argparse import ArgumentParser
-from copy import deepcopy
 from glob import glob
 from pathlib import Path
 
-import psutil
-import ray
 import renderer
-from renderer.tools import components
-
-import sort_output
 
 
-def read_file(path) -> dict:  # extract from JSON as dict
-    with open(path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        return data
-
-def get_batch(ns: list[str]) -> int:
+def get_batch(ns: str) -> int:
     with open("batchprogress.json", "r") as f:
         data = json.load(f)
-        return data.get(','.join(ns), -1)
+        return data.get(ns, -1)
 
 def set_batch(ns: str, batch: int):
     with open("batchprogress.json", "r+") as f:
         data = json.load(f)
         if batch != -1:
-            data[','.join(ns)] = batch
+            data[ns] = batch
         else:
-            del data[','.join(ns)]
+            del data[ns]
         f.seek(0)
         f.truncate()
         json.dump(data, f, indent=2)
+
+def read_file(path) -> dict:  # extract from JSON as dict
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        return data
 
 
 def main():
@@ -57,29 +50,25 @@ def main():
         rendered_node_json = all_node_json
         rendered_comps_json = all_comps_json
 
-    all_nodes = renderer.NodeList(all_node_json)
-    all_comps = renderer.ComponentList(all_comps_json, all_node_json)
     rendered_nodes = renderer.NodeList(rendered_node_json)
     rendered_comps = renderer.ComponentList(rendered_comps_json, rendered_node_json)
 
     print(f"Rendering {', '.join(args.namespaces) or 'everything'}")
+    zoom = renderer.ZoomParams(0, 9, 32)
+    export_id = ",".join(args.namespaces) if args.namespaces else "all"
 
-    tiles = components.rendered_in(rendered_comps, rendered_nodes, 0, 9, 32)
-    print(f"{len(tiles)} tiles to render")
-
-    last_batch = get_batch(args.namespaces)
-
-    for i, batch in enumerate(tiles[x:x + 1000] for x in range(0, len(tiles), 1000)):
-        if i <= last_batch: continue
-        print(f"Batch {i} of {int(len(tiles) // 1000)}")
-        renderer.render(deepcopy(all_comps), deepcopy(all_nodes), renderer.ZoomParams(0, 9, 32),
-                        save_dir=Path("./tiles"), offset=renderer.Coord(0, 32),
-                        tiles=batch, use_ray=True, processes=psutil.cpu_count()//2)
-
-        sort_output.main()
-        ray.shutdown()
-        gc.collect()
-        set_batch(args.namespaces, i)
-    set_batch(args.namespaces, -1)
+    if get_batch(export_id) == -1:
+        renderer.base.prepare_render(rendered_comps, rendered_nodes, zoom, export_id,
+                                     offset=renderer.Coord(0, 32))
+        set_batch(export_id, 0)
+    if get_batch(export_id) == 0:
+        renderer.base.render_part1_ray(rendered_comps, rendered_nodes, zoom, export_id, batch_size=8)
+        set_batch(export_id, 1)
+    if get_batch(export_id) == 1:
+        renderer.base.render_part2(export_id)
+        set_batch(export_id, 2)
+    if get_batch(export_id) == 2:
+        renderer.base.render_part3_ray(export_id, save_dir=Path("./tiles"))
+        set_batch(export_id, -1)
 
 if __name__ == "__main__": main()
