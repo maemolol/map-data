d8d4f7cd64489c91d82aa05faa9358f106e140fa
7d <61975820+iiiii7d@users.noreply.github.com>
2023-04-16 16:06:44 +0800

    update script for new version

--- render.py
+++ render.py
@@ -3,30 +3,12 @@ from argparse import ArgumentParser
 from glob import glob
 from pathlib import Path
 
-import ray
-import vector
+import renderer.render
 import psutil
-from renderer.render import prepare_render, render_part1, render_part2, render_part3
-from renderer.types.pla2 import Pla2File
-from renderer.types.zoom_params import ZoomParams
-
-
-def get_batch(ns: str) -> int:
-    with open("batchprogress.json", "r") as f:
-        data = json.load(f)
-        return data.get(ns, -1)
-
-
-def set_batch(ns: str, batch: int):
-    with open("batchprogress.json", "r+") as f:
-        data = json.load(f)
-        if batch != -1:
-            data[ns] = batch
-        else:
-            del data[ns]
-        f.seek(0)
-        f.truncate()
-        json.dump(data, f, indent=2)
+from renderer import Vector, MultiprocessConfig
+from renderer.misc_types.pla2 import Pla2File
+from renderer.misc_types.config import Config
+from renderer.misc_types.zoom_params import ZoomParams
 
 
 def main():
@@ -43,29 +25,27 @@ def main():
         for file in glob("files/*"):
             renders.extend(Pla2File.from_file(Path(file)).components)
     renders = list({(c.namespace, c.id): c for c in renders}.values())
-    zoom = ZoomParams(0, 9, 32)
     renders = Pla2File(
         namespace="",
         components=renders
     )
 
-    print(f"Rendering {', '.join(args.namespaces) or 'everything'}")
-    export_id = ",".join(args.namespaces) if args.namespaces else "all"
+    config = Config(
+        zoom=ZoomParams(0, 9, 32),
+        temp_dir=Path("./temp"),
+        export_id=",".join(args.namespaces) if args.namespaces else "all"
+    )
 
-    ray.init(num_cpus=psutil.cpu_count())
-    if get_batch(export_id) == -1:
-        prepare_render(renders, zoom, export_id,
-                       offset=vector.obj(x=0, y=32), zooms=args.zooms or None)
-        set_batch(export_id, 0)
-    if get_batch(export_id) == 0:
-        render_part1(zoom, export_id, batch_size=psutil.cpu_count(), chunk_size=4)
-        set_batch(export_id, 1)
-    if get_batch(export_id) == 1:
-        render_part2(export_id, Path("./temp"))
-        set_batch(export_id, 2)
-    if get_batch(export_id) == 2:
-        render_part3(export_id, save_dir=Path("./tiles"), batch_size=2*psutil.cpu_count())
-        set_batch(export_id, -1)
+    print(f"Rendering {', '.join(args.namespaces) or 'everything'}")
+
+    renderer.render(
+        renders,
+        config,
+        save_dir=Path("./tiles"),
+        offset=Vector(0, 32),
+        zooms=args.zooms or None,
+        part3_mp_config=MultiprocessConfig(batch_size=2*psutil.cpu_count())
+    )
 
 
 if __name__ == "__main__":
